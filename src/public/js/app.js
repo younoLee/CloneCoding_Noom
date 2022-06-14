0fcf61137b9145a7699f2f1cb59ebc0d80d8ec80
es2646526@gmail.com <es2646526@gmail.com>
2022-06-14 18:36:16 +0900

    noom_ws_event

---
+++ src/public/js/app.js
@@ -0,0 +1,3 @@
+// alert("hi!");
+// const socket= new WebSocket("http://localhost:3000");
+const socket = new WebSocket(`ws://${window.location.host}`);
--- src/server.js
+++ src/server.js
@@ -3,8 +3,6 @@ import http from "http";
 import WebSocket from "ws";
 import express from "express";
 
-
-
 const app = express();
 
 app.set("view engine","pug");
@@ -13,9 +11,15 @@ app.use("/public",express.static(__dirname+"/public"))
 app.get("/",(req,res) => res.render("home"));
 app.get("/*",(req,res) => res.redirect("/"));
 
-const handleListen = () => console.log("Listenling http://localhost:3000");
+const handleListen = () => console.log("Listening http://localhost:3000");
 // app.listen(3000,handleListen);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+function handleConnection(socket){
+    console.log(socket);
+};
+
+wss.on("connection", handleConnection);
 
 server.listen(3000,handleListen);
