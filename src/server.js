78d6c64d2fe0dc2d8c86b98bed9c84cb1f9f73c0
es2646526@gmail.com <es2646526@gmail.com>
2022-06-16 17:46:08 +0900

    noom_websocket

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 import http from "http";
 import WebSocket from "ws";
 import express from "express";
+import { Socket } from "dgram";
 
 const app = express();
 
@@ -16,10 +17,22 @@ const handleListen = () => console.log("Listening http://localhost:3000");
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket){
-    console.log(socket);
-};
+//function handleConnection(socket){
+//    console.log(socket);
+//};
 
-wss.on("connection", handleConnection);
+//wss.on("connection", handleConnection);
+
+wss.on("connection",(socket) =>{
+    // console.log(socket)
+    console.log("Connected to Browser");
+    socket.on("close", () => console.log("Disconnected from Browser"));
+    socket.on("message", (message) => {
+     console.log(`${message}`);
+    });
+    socket.send("hello!!");
+
+});
+
 
 server.listen(3000,handleListen);
